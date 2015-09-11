4fa7817ca6f81cc94b3e117109b4f7a0ff9f155c
Aldrog <penkrat8@gmail.com>
2015-09-11 15:16:54 +0300

    Implemented interpolation.

--- main.nim
+++ main.nim
@@ -6,8 +6,10 @@ import interpolation
 const 
     a = -1.0
     b = 1.0
-    randomPoints1 = [-1.0, -0.5, 0.0, 0.5, 1.0]
-    randomPoints2 = [-1.0, -0.86, -0.71, -0.57, -0.43, -0.28, -0.14, 0.0, 0.14, 0.28, 0.43, 0.57, 0.71, 0.86, 1.0]
+    n1 = 5
+    n2 = 15
+    randomPoints1 = @[-1.0, -0.5, 0.0, 0.5, 1.0]
+    randomPoints2 = @[-1.0, -0.86, -0.71, -0.57, -0.43, -0.28, -0.14, 0.0, 0.14, 0.28, 0.43, 0.57, 0.71, 0.86, 1.0]
 
 proc values(f: proc(x: float64): float64, 
             x: varargs[float64]): seq[float64] = 
