290293c87efbca48a9d4fb8659fcffbb55811a0e
Aldrog <penkrat8@gmail.com>
2015-09-15 14:31:28 +0300

    Plotting graph with 'graphics' module, float64 -> float.

--- interpolation.nim
+++ interpolation.nim
@@ -1,11 +1,11 @@
-proc lm(i: int, xk: seq[float64], x: float64): float64 = 
+proc lm(i: int, xk: seq[float], x: float): float = 
     result = 1
     for j in low(xk)..high(xk):
         if(i != j):
             result = result * (x - xk[j]) / (xk[i] - xk[j])
 
-proc interpolate*(px, py: seq[float64]): (proc(x: float64): float64) = 
-    proc res(x: float64): float64 = 
+proc interpolate*(px, py: seq[float]): (proc(x: float): float) = 
+    proc res(x: float): float = 
         for i in low(px)..high(px):
             result = result + py[i] * lm(i, px, x)
     return res
--- main.nim
+++ main.nim
@@ -1,8 +1,5 @@
-import math
 import strutils
-import NimQml
-import macros
-import typeinfo
+import sdl, graphics, colors
 import functions
 import interpolation
 
@@ -14,11 +11,8 @@ const
     randomPoints1 = @[-1.0, -0.5, 0.0, 0.5, 1.0]
     randomPoints2 = @[-1.0, -0.86, -0.71, -0.57, -0.43, -0.28, -0.14, 0.0, 0.14, 0.28, 0.43, 0.57, 0.71, 0.86, 1.0]
 
-type resultContainer = ref object of QObject
-    
-
-proc values(f: proc(x: float64): float64, 
-            x: varargs[float64]): seq[float64] = 
+proc values(f: proc(x: float): float, 
+            x: varargs[float]): seq[float] = 
     result.newSeq(x.len)
     for i in low(x)..high(x):
         result[i] = f(x[i])
@@ -30,23 +24,45 @@ for p in randomPoints1:
 stdout.write("\n|")
 for p in rp1Values:
     stdout.write(formatFloat(f = p, precision = 4), "\t|")
-stdout.write("\n")
+stdout.write("\n\n")
 
-let interpolated1 = interpolate(randomPoints1, rp1Values)
+let res1 = interpolate(randomPoints1, rp1Values)
 
-proc openWindow() = 
-    var app = newQApplication()
-    defer: app.delete()
-    
-    var rc = resultContainer()
-    defer: rc.delete()
-    
-    var engine = newQQmlApplicationEngine()
-    defer: engine.delete()
-    
-    #var rootContext: QQmlContext = engine.rootContext()
-    
-    engine.load("mainWindow.qml")
-    app.exec()
+var checkPoints: seq[float]
+checkPoints = @[]
+const cpCount = 100
+var tp = a
+for i in 0..cpCount:
+    checkPoints.add(tp)
+    tp = tp + (b - a) / cpCount.toFloat
+let f1Vals = f1.values(checkPoints)
+let res1Vals = res1.values(checkPoints)
 
-when isMainModule: openWindow()
+# Graphics
+
+const
+    width = 600
+    height = 800
+var surf = newScreenSurface(width, height)
+surf.fillSurface(colWhite)
+
+const
+    xoffset = 1.0
+    yoffset = 1.0
+let
+    scale = width / (checkPoints[checkPoints.high] - checkPoints[checkPoints.low])
+
+# Axis
+surf.drawLine(((xoffset * scale).int, 0), ((xoffset * scale).int, height), colDarkGray)
+surf.drawLine((0, (yoffset * scale + height).int), (width, (yoffset * scale + height).int), colDarkGray)
+
+for i in 1..cpCount:
+    surf.drawLine((((checkPoints[i-1] + xoffset) * scale).int, ((yoffset - f1Vals[i-1]) * scale + height).int), (((checkPoints[i] + xoffset) * scale).int, ((yoffset - f1Vals[i]) * scale + height).int), colBlue)
+    surf.drawLine((((checkPoints[i-1] + xoffset) * scale).int, ((yoffset - res1Vals[i-1]) * scale + height).int), (((checkPoints[i] + xoffset) * scale).int, ((yoffset - res1Vals[i]) * scale + height).int), colRed)
+
+withEvents(surf, event):
+    var eventp = addr(event)
+    if event.kind == sdl.QUITEV:
+        break
+    sdl.updateRect(surf.s, 0, 0, width, height)
+    
