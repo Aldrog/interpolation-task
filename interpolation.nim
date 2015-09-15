ebc404c4a76e1e5cb32173a8e65ef55f9853ce67
Aldrog <penkrat8@gmail.com>
2015-09-15 11:15:28 +0300

    Fix interpolate function.

--- interpolation.nim
+++ interpolation.nim
@@ -1,18 +1,11 @@
-discard """
-proc sum(F: varargs[proc(x: float64): float64]): proc(x: float64): float64 = 
-    result = proc(x: float64): float64 = return 0
-    for f in F:
-        var tmp = result
-        result = proc(x: float64): float64 = return tmp(x) + f(x)
-"""
-
-proc lm(i: int, xk: varargs[float64], x: float64): float64 = 
+proc lm(i: int, xk: seq[float64], x: float64): float64 = 
     result = 1
     for j in low(xk)..high(xk):
-        result = result * (x - xk[j]) / (xk[i] - xk[j])
+        if(i != j):
+            result = result * (x - xk[j]) / (xk[i] - xk[j])
 
-proc interpolate*(px, py: seq[float64]): proc(x: float64): float64 = 
-    result = proc(px: float64): float64 = return 0
-    for i in low(px)..high(px):
-        var restmp = result
-        result = proc(x: float64): float64 = return restmp(x) + py[i] * lm(i, px, x)
+proc interpolate*(px, py: seq[float64]): (proc(x: float64): float64) = 
+    proc res(x: float64): float64 = 
+        for i in low(px)..high(px):
+            result = result + py[i] * lm(i, px, x)
+    return res
